1f4488679f03b4302c9fc3bbdeaa345dc70a6e5a
Luiz Branco <me@luizbranco.com>
2014-12-08 21:37:36 -0500

    Read from file and print to screen

--- linux_maze.cpp
+++ linux_maze.cpp
@@ -7,16 +7,44 @@
 #define internal static
 
 internal void PlatformReadMapFromFile(const char* fileName, map* maze);
+internal void PlatformRenderMap(map* maze);
 
 int main() {
   map maze;
   maze.rows = 6;
-  maze.cols = 15;
+  maze.cols = 45;
   PlatformReadMapFromFile("map1.txt", &maze);
-  printf("%d\n", sizeof(maze.tiles));
+  PlatformRenderMap(&maze);
   return 0;
 }
 
 internal void PlatformReadMapFromFile(const char* fileName, map* maze) {
-  maze->tiles = (uint8_t*)calloc(maze->rows * maze->cols, sizeof(uint8_t));
+  FILE* fp;
+  int i;
+  int size = maze->rows * maze->cols;
+  maze->tiles = (uint8_t*)calloc(size, sizeof(uint8_t));
+  char c;
+  fp = fopen(fileName, "r");
+  for(;;) {
+    c = fgetc(fp);
+    if(i >= size || feof(fp)) {
+      break;
+    }
+    if (c != '\n') {
+      maze->tiles[i] = c;
+      ++i;
+    }
+  }
+  fclose(fp);
+}
+
+internal void PlatformRenderMap(map* maze) {
+  int i;
+  for (i = 0; i < maze->rows*maze->cols; i++) {
+    if (i > 0 && i % maze->cols == 0) {
+      printf("\n");
+    }
+    printf("%c", maze->tiles[i]);
+  }
+  printf("\n");
 }
